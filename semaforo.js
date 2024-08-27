c90d4fb245fdfaacff03b65724910bd34f345535
Pablo_Eduardo <141835581+pablo041@users.noreply.github.com>
2024-08-27 08:05:11 -0300

    f

--- semaforo.js
+++ semaforo.js
@@ -5,24 +5,7 @@ let intervalId = null;
 
 const trafficLight = ( event ) => {
     stopAutomatic();
-    tuconst img = document.getElementById( 'img' );
-    const buttons = document.getElementById( 'buttons' );
-    let colorIndex = 0;
-    let intervalId = null;
-    
-    const trafficLight = ( event ) => {
-        stopAutomatic();
-        turnOn[event.target.id]();
-    }
-    
-    …    },
-        'stop': () => {
-            stopAutomatic()
-            buttons.children.stop.id = 'automatic';
-        }
-    }
-    
-    buttons.addEventListener('click', trafficLight );rnOn[event.target.id]();
+    turnOn[event.target.id]();
 }
 
 const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
