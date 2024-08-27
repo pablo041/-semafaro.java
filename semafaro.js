61ac2cb9d83913ea75069c2b9c622799e413caa6
Pablo_Eduardo <141835581+pablo041@users.noreply.github.com>
2024-08-27 08:02:21 -0300

    n

--- semafaro.js
+++ semafaro.js
@@ -5,7 +5,24 @@ let intervalId = null;
 
 const trafficLight = ( event ) => {
     stopAutomatic();
-    turnOn[event.target.id]();
+    tuconst img = document.getElementById( 'img' );
+    const buttons = document.getElementById( 'buttons' );
+    let colorIndex = 0;
+    let intervalId = null;
+    
+    const trafficLight = ( event ) => {
+        stopAutomatic();
+        turnOn[event.target.id]();
+    }
+    
+    …    },
+        'stop': () => {
+            stopAutomatic()
+            buttons.children.stop.id = 'automatic';
+        }
+    }
+    
+    buttons.addEventListener('click', trafficLight );rnOn[event.target.id]();
 }
 
 const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
@@ -21,7 +38,7 @@ const stopAutomatic = () => {
     clearInterval ( intervalId );
 }
 
-const turnOn = {Automático
+const turnOn = {
     'red':      () => img.src = './img/vermelho.png',
     'yellow':   () => img.src = './img/amarelo.png',
     'green':    () => img.src = './img/verde.png',
